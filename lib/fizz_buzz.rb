078ecf98c9459a66f9cf24a1d68848cfcfcf018b
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-06-19 01:41:58 +0000

    fizz_buzz finish

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,1 +1,26 @@
-puts 'Hello, World!'
+def fizz_buzz(n)
+  if n % 15 == 0
+    'Fizz Buzz'
+  elsif n % 3 == 0
+    'Fizz'
+  elsif n % 5 == 0
+    'Buzz'
+  else
+    n.to_s
+  end
+
+end
+
+# puts fizz_buzz(1)
+# puts fizz_buzz(2)
+# puts fizz_buzz(3)
+# puts fizz_buzz(4)
+# puts fizz_buzz(5)
+# puts fizz_buzz(6)
+# puts fizz_buzz(15)
+
+puts '数字を入力して下さい'
+input = gets.to_i
+
+puts '結果は...'
+puts fizz_buzz(input)
