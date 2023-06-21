26864b178ab48640737162c1070beabe61719526
narumifumiya <124240213+narumifumiya@users.noreply.github.com>
2023-06-21 03:02:21 +0000

    fizz_buzz minitest実施

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -19,8 +19,9 @@ end
 # puts fizz_buzz(6)
 # puts fizz_buzz(15)
 
-puts '数字を入力して下さい'
-input = gets.to_i
+# puts '数字を入力して下さい'
+# input = gets.to_i
 
-puts '結果は...'
-puts fizz_buzz(input)
+# puts '結果は...'
+# puts fizz_buzz(input)
+
